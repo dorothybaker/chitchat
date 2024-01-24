e079b4c3e09e16eadeeb7fcc200b71efebf205c3
Dorothy Baker <tbakerdorothy@gmail.com>
2024-01-24 17:02:56 +0300

    update

--- app/components/MessageInput.jsx
+++ app/components/MessageInput.jsx
@@ -64,70 +64,74 @@ export default function MessageInput({
   };
 
   return (
-    <div className="flex items-center px-2 py-4 border-t bg-background gap-1 sticky bottom-0 w-full overflow-hidden">
-      <div>
-        <Paperclip
-          className={`cursor-pointer ${
-            image ? "text-primary" : "text-foreground"
-          }`}
-          size={15}
-          onClick={() => document.getElementById("my_modal_3").showModal()}
-        />
-      </div>
-      <button
-        onClick={() => setShowEmojiPicker(!showEmojiPicker)}
-        className="relative"
-      >
-        😊
-      </button>
+    <div className="sticky bottom-0">
+      <div className="flex items-center px-2 py-4 border-t bg-background gap-1 relative w-full overflow-hidden justify-between">
+        <div className="flex items-center gap-1 flex-auto">
+          <div>
+            <Paperclip
+              className={`cursor-pointer ${
+                image ? "text-primary" : "text-foreground"
+              }`}
+              size={15}
+              onClick={() => document.getElementById("my_modal_3").showModal()}
+            />
+          </div>
+          <button
+            onClick={() => setShowEmojiPicker(!showEmojiPicker)}
+            className="relative"
+          >
+            😊
+          </button>
 
-      <input
-        type="text"
-        className="flex-1 outline-none focus:outline-none border-none text-foreground"
-        placeholder="Type a message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <div>
-        <Send className="cursor-pointer text-primary" onClick={sendMessage} />
-      </div>
-
-      {showEmojiPicker && (
-        <div className="absolute right-0 bottom-full p-2">
-          <EmojiPicker onEmojiClick={handleEmojiClick} width="100%" />
+          <input
+            type="text"
+            className="sm:flex-1 outline-none focus:outline-none border-none text-foreground w-full"
+            placeholder="Type a message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
         </div>
-      )}
-      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            {imagePreview && (
-              <img
-                src={imagePreview}
-                className="w-60 max-h-60 object-cover rounded-md my-2 block"
-              />
-            )}
-            <input type="file" onChange={handleFileChange} accept="image/*" />
-            <button
-              className="rounded-md bg-primary text-background py-1.5 px-3 w-max my-2 cursor-pointer"
-              onClick={() => handleUpload()}
-            >
-              Upload
-            </button>
-
-            <div>
-              <progress
-                value={uploadProgress}
-                max="100"
-                className="rounded-sm"
-              ></progress>
-            </div>
-
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-              ✕
-            </button>
-          </form>
+        <div className="flex-1">
+          <Send className="cursor-pointer text-primary" onClick={sendMessage} />
         </div>
-      </dialog>
+
+        {showEmojiPicker && (
+          <div className="absolute right-0 bottom-full p-2">
+            <EmojiPicker onEmojiClick={handleEmojiClick} width="100%" />
+          </div>
+        )}
+        <dialog id="my_modal_3" className="modal">
+          <div className="modal-box">
+            <form method="dialog">
+              {imagePreview && (
+                <img
+                  src={imagePreview}
+                  className="w-60 max-h-60 object-cover rounded-md my-2 block"
+                />
+              )}
+              <input type="file" onChange={handleFileChange} accept="image/*" />
+              <button
+                className="rounded-md bg-primary text-background py-1.5 px-3 w-max my-2 cursor-pointer"
+                onClick={() => handleUpload()}
+              >
+                Upload
+              </button>
+
+              <div>
+                <progress
+                  value={uploadProgress}
+                  max="100"
+                  className="rounded-sm"
+                ></progress>
+              </div>
+
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+            </form>
+          </div>
+        </dialog>
+      </div>
     </div>
   );
 }
