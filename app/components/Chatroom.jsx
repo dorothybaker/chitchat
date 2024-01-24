5caa9d2d9285c6b57726c1a0d0a659e1df08091d
Dorothy Baker <tbakerdorothy@gmail.com>
2024-01-24 20:33:36 +0300

    padding

--- app/components/Chatroom.jsx
+++ app/components/Chatroom.jsx
@@ -87,9 +87,9 @@ export default function Chatroom({ user, selectedChatroom }) {
       ref={messagesContainerRef}
     >
       {selectedChatroom ? (
-        <div className="flex-1 flex flex-col gap-5 sm:px-4 px-2 pb-20 w-full">
+        <div className="flex-1 flex flex-col gap-5 sm:px-4 px-2 py-20 w-full">
           {selectedChatroom && (
-            <div className="border-b fixed top-0 mb-20 flex gap-2 items-center p-2 bg-background z-40 w-full">
+            <div className="border-b fixed top-0 flex gap-2 items-center p-2 bg-background z-40 w-full">
               <div className="h-10 w-10">
                 <img
                   src={other.avatarUrl}
